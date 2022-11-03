bf0bf1001a19c08de4c188b3cab7412548e02311
Sacha Bron <me@sachabron.ch>
2022-11-04 00:39:20 +0100

    Add Clear Screen to the experiment

--- telnet-test.py
+++ telnet-test.py
@@ -12,27 +12,37 @@ soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 clients = []
 
 def accept_new_connection(soc, mask):
-    connection, address = soc.accept()
+    conn, address = soc.accept()
     print(f'New connection ({len(clients) + 1}) from: {address[0]}')
-    connection.setblocking(False)
-    clients.append(connection)
-    sel.register(connection, selectors.EVENT_READ, read_client_data)
+    conn.setblocking(False)
+    clients.append(conn)
+    sel.register(conn, selectors.EVENT_READ, read_client_data)
 
-def read_client_data(connection, mask):
-    data = connection.recv(1000)
+def read_client_data(conn, mask):
+    data = conn.recv(1000)
     if data:
-        print(data)
-        # connection.send(data) # Hope it won't block
-        broadcast(data)
+        # conn.send(data) # Hope it won't block
+        try:
+            str = data.decode('utf-8').strip()
+            print(str)
+            if str == "clear":
+                clear_screen(conn)
+            else:
+                broadcast(str + '\r\n')
+        except UnicodeDecodeError as e:
+            print("Control character received", data)
     else:
         print('Good bye!')
-        sel.unregister(connection)
-        clients.remove(connection)
-        connection.close()
+        sel.unregister(conn)
+        clients.remove(conn)
+        conn.close()
 
 def broadcast(msg):
     for client in clients:
-        client.send(msg)
+        client.send(msg.encode('utf-8'))
+
+def clear_screen(conn):
+    conn.send("\033[2J".encode('utf-8'))
 
 def main():
     soc.bind((HOST, PORT))
@@ -50,7 +60,5 @@ def main():
 
 try:
     main()
-except Exception as e:
-    print(str(e))
 finally:
     soc.close()
